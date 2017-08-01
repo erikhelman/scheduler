d661af16900a92f1053a57954f9227c3baced70a
Erik Helman <erik.helman@outlook.com>
2017-08-01 11:15:21 -0400

    New files

--- home.py
+++ home.py
@@ -10,28 +10,31 @@ db = SQLAlchemy(app)
 class Students(db.Model):
     ___tablename___ = 'students'
     id = db.Column('student_id', db.Integer, primary_key = True)
-    student_fname = db.Column(db.String(50))
-    student_lname = db.Column(db.String(50))
+    fname = db.Column(db.String(50))
+    lname = db.Column(db.String(50))
     gender = db.Column(db.String(1))
     email = db.Column(db.String(50))
     city = db.Column(db.String(50))
-    street_addr = db.Column(db.String(200))
+    province = db.Column(db.String(2))
+    street_address = db.Column(db.String(200))
     postal = db.Column(db.String(6))
-    current_level = db.Column(db.Integer)
-    prev_level = db.Column(db.Integer)
+    phone_type = db.Column(db.String(6))
+    area_code = db.Column(db.Integer)
+    phone_number = db.Column(db.Integer)
 
-
-    def __init__(self, student_fname, student_lname, gender, email, city, street_addr, postal,
-             current_level, prev_level):
-        self.student_fname = student_fname
-        self.student_lname = student_lname
+    def __init__(self, fname, lname, gender, email, city, province, street_address, postal,
+             phone_type, area_code, phone_number):
+        self.fname = fname
+        self.lname = lname
         self.gender = gender
         self.email = email
         self.city = city
-        self.street_addr = street_addr
+        self.province = province
+        self.street_address = street_address
         self.postal = postal
-        self.current_level = current_level
-        self.prev_level = prev_level
+        self.phone_type = phone_type
+        self.area_code = area_code
+        self.phone_number = phone_number
 
 
 @app.route('/')
@@ -49,6 +52,22 @@ def scheduler_json():
 @app.route('/students')
 def get_students():
     return render_template('students.html', students = Students.query.all())
+
+@app.route('/delete', methods=['GET', 'POST'])
+def delete_students():
+    if request.method == 'POST':
+        delete_name = request.form['delete_name']
+        Students.query.filter(Students.fname == delete_name).delete()
+        db.session.commit()
+    return render_template('delete.html')
+
+@app.route('/search', methods=['GET', 'POST'])
+def search_students():
+    if request.method == 'POST':
+        search_string = request.form['search_string']
+        return render_template('search.html', students = Students.query.filter((Students.lname == search_string)|(Students.fname == search_string)).all())
+
+    return render_template('search.html')
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -70,10 +89,12 @@ def add_students():
                            request.form['gender'],
                            request.form['email'],
                            request.form['city'],
-                           request.form['street_addr'],
+                           request.form['province'],
+                           request.form['streetAddress'],
                            request.form['postal'],
-                           request.form['curr_level'],
-                           request.form['prev_level'])
+                           request.form['phoneType'],
+                           request.form['areaCode'],
+                           request.form['phoneNumber'])
 
         db.session.add(student)
         db.session.commit()
